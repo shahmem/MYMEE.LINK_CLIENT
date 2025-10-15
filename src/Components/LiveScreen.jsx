fe1a4a31cbc1822b606352086ea6c15c19157b3a
shahmem <kpmshameemudheenkp@gmail.com>
2025-10-15 11:35:08 +0530

    last commit

--- src/Components/LiveScreen.jsx
+++ src/Components/LiveScreen.jsx
@@ -5,10 +5,10 @@ function LiveScreen({ links, user, socialLinks, theme, position }) {
   return (
     <div
       style={{ fontFamily: theme.fontStyle }}
-      className="flex-1 p-6 w-[45%] hidden  md:flex justify-center items-center md:pl-24 md:pr-12 md:py-12"
+      className="flex-1 p-6 w-[45%] hidden  md:flex justify-center items-center md:pl-24 md:pr-12 md:pt-28"
     >
       <div
-        className={` md:border-[12px] border-8 border-gray-800 min-w-[17rem] h-[34rem] rounded-[2rem]`}
+        className={` md:border-[12px] border-8 border-gray-800 w-[17rem] h-[34rem] rounded-[2rem]`}
       >
       <PublicScreen style={{ height: "100%" }} className="rounde-4xl" links={links} user={user} socialLinks={socialLinks} theme={theme} position={position} />
         
